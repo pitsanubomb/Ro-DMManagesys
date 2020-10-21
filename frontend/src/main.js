741dcdd03a79c10afd7150b55ff8cc82f32b9b30
pitsanubomb <pitsanubomb@gmail.com>
2020-10-21 22:28:09 +0700

    :fire: Add firebase auth :fire:

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,26 @@
 import { createApp } from 'vue';
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
+import firebase from 'firebase/app';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import '@firebase/auth';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBPrhbCDpaXyVn4w0mtrRk1jxpVpl4WGFU',
+  authDomain: 'dm-manage.firebaseapp.com',
+  databaseURL: 'https://dm-manage.firebaseio.com',
+  projectId: 'dm-manage',
+  storageBucket: 'dm-manage.appspot.com',
+  messagingSenderId: '55591847257',
+  appId: '1:55591847257:web:fc4e95479d772dbabbb848',
+  measurementId: 'G-WGY3YLN8KK',
+};
+
+firebase.initializeApp(firebaseConfig);
 
 createApp(App)
   .use(Antd)
