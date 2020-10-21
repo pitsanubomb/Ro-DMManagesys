7e6d6c5b2b2d56040f66b96ce9963edeebdb2e1f
pitsanubomb <pitsanubomb@gmail.com>
2020-10-21 21:28:07 +0700

    :fire: Re fix add UI :rocket:

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,10 @@
 import { createApp } from 'vue';
 import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
-import 'ant-design-vue/dist/antd.css';
 
 createApp(App)
   .use(Antd)
